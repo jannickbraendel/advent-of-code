06e44b415aadf4ba65890321e450363b3985869f
jannickbraendel <jannick2@braendel.de>
2024-12-21 15:27:51 +0100

    day 16 completed

--- 2024/16/16.py
+++ 2024/16/16.py
@@ -41,11 +41,15 @@ def get_maze(i, j):
     return maze[i][j]
 
 
-def find_shortest_path(pos: (int, int), direction: Direction, current_path: [((int, int), Direction)]):
-    global paths, blocked
+def find_shortest_paths(pos: (int, int), direction: Direction, current_path: [((int, int), Direction)], current_score: int):
+    global shortest_paths, blocked, min_score
     if pos == end_pos:
         # found end
-        paths.append(current_path)
+        if current_score <= min_score:
+            shortest_paths.append((current_path, current_score))
+            min_score = current_score
+            print("updated min score")
+        print("found path")
         return
 
     blocked.add(pos)
@@ -58,39 +62,50 @@ def find_shortest_path(pos: (int, int), direction: Direction, current_path: [((i
         if get_maze(nx, ny) == "#" or next_tile in blocked:
             continue
 
-        print(f"next: {next_tile}, direction: {d}, maze-character: {get_maze(nx, ny)}")
-        find_shortest_path(next_tile, d, current_path + [(next_tile, d)])
+        # update current score and check if it's above the current min score
+        new_score = current_score + 1
+        if d != direction:
+            # made turn
+            new_score += 1000
+
+        if new_score > min_score:
+            continue
+
+        if ((nx, ny), d) in visited and visited[((nx, ny), d)] < new_score:
+            # visited before with lower score -> can be discarded
+            print("visited this node before")
+            continue
+
+        visited[((nx, ny), d)] = new_score
+
+        print(f"next: {next_tile}, direction: {d}, score: {new_score}")
+        find_shortest_paths(next_tile, d, current_path + [(next_tile, d)], new_score)
 
     blocked.remove(pos)
 
 
-def score(path):
-    turns = 0
-    current_direction = Direction.RIGHT
-    for (_, direction) in path:
-        if direction != current_direction:
-            turns += 1
-            current_direction = direction
-
-    steps = set([pos for (pos, _) in path])
-
-    return len(steps) - 1 + 1000 * turns
+def filter_shortest_paths():
+    global shortest_paths
+    shortest_paths = [(path, score) for (path, score) in shortest_paths if score <= min_score]
 
 
-def minimum_score():
-    result = inf
-    for path in paths:
-        s = score(path)
-        result = min(s, result)
-
+def find_best_sitting_spots():
+    result = set()
+    for (path, _) in shortest_paths:
+        for (pos, _) in path:
+            result.add(pos)
     return result
 
 
 maze, start_pos, end_pos = parse("day16.txt")
 # cells that are part of the current path buffer
 blocked = set()
-paths = []
+shortest_paths: [(((int, int), Direction), int)] = []
+visited: {((int, int), Direction): int} = dict()
+visited[(start_pos, Direction.RIGHT)] = 0
+min_score = float(inf)
 
-find_shortest_path(start_pos, Direction.RIGHT, [(start_pos, Direction.RIGHT)])
-print(minimum_score())
-
+find_shortest_paths(start_pos, Direction.RIGHT, [(start_pos, Direction.RIGHT)], 0)
+filter_shortest_paths()
+print(f"Spots: {find_best_sitting_spots()}")
+print(f"Amount of Sitting Spots: {len(find_best_sitting_spots())}")
