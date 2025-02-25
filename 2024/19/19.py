1507bde03084b3f160735160821c5db79dc07931
jannickbraendel <jannick2@braendel.de>
2025-02-25 17:44:34 +0100

    day 19 inefficient BFS

--- 2024/19/19.py
+++ 2024/19/19.py
@@ -1,4 +1,3 @@
-
 def parse(file):
     file = open(file, "r")
     lines = file.readlines()
@@ -8,54 +7,44 @@ def parse(file):
     return patterns_local, designs_local
 
 
-def design_pattern_match(design):
-    for p in patterns:
-        if design == p:
+class TowelNode:
+    def __init__(self, pattern):
+        self.pattern = pattern
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+
+def search_design(design):
+    print(f"design: {design}")
+    root = TowelNode("")
+    queue = [root]
+    while len(queue) > 0:
+        current = queue.pop(0)
+
+        if current.pattern == design:
             return True
+        print(f"queue: {len(queue)}")
+
+        child_patterns = [(current.pattern + p) for p in patterns]
+        for cp in child_patterns:
+            if not design.startswith(cp):
+                continue
+            node = TowelNode(cp)
+            current.add_child(node)
+            queue.append(node)
+
     return False
-
-
-# check if design is a prefix of a pattern
-def design_pattern_prefix_match(design):
-    for p in patterns:
-        if p.startswith(design):
-            return True
-    return False
-
-
-
-def check_design(design):
-    print(f"checking (sub)-design {design}")
-    if len(design) == 0:
-        # whole design matches
-        return True
-
-    if len(design) == 1:
-        return design_pattern_match(design)
-
-    cur_design = ""
-    prefix_length = 1
-    while True:
-        # add letters to current design and check if it corresponds to pattern
-        cur_design = design[:prefix_length]
-
-        if design_pattern_prefix_match(cur_design):
-            # pattern starts with current sub design -> check next color
-            prefix_length += 1
-        else:
-            prefix_length -= 1
-            break
-
-    if not design_pattern_match(design[:prefix_length]):
-        # prefix is not matching at all
-        return False
-
-    return check_design(design[prefix_length:])
 
 
 patterns, designs = parse("day19-small.txt")
+
+
 possible_designs = 0
-for d in designs:
-    possible_designs += check_design(d)
+for i, d in enumerate(designs):
+    print(i)
+    possible_designs += search_design(d)
 
 print(possible_designs)
+
